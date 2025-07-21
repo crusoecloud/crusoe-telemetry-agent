88586a8bf3a1e15062ae8570b02855d6d99639bf
Ashish Rathi <arathi@crusoeenergy.com>
2025-07-20 17:15:44 -0700

    updated setup and docker compose to remove intermittent failures

--- setup_beacon.sh
+++ setup_beacon.sh
@@ -9,19 +9,15 @@ GITHUB_RAW_BASE_URL="https://raw.githubusercontent.com/crusoecloud/beacon/main"
 
 # Define paths for config files within the GitHub repository
 REMOTE_VECTOR_CONFIG_GPU_VM="config/vector_gpu_vm.yaml"
+REMOTE_VECTOR_CONFIG_CPU_VM="config/vector_cpu_vm.yaml"
 REMOTE_DCGM_EXPORTER_METRICS_CONFIG="config/dcp-metrics-included.csv"
 REMOTE_DOCKER_COMPOSE_GPU_VM_UBUNTU_22="docker/docker-compose-gpu-vm-ubuntu22.04.yaml"
+REMOTE_DOCKER_COMPOSE_CPU_VM="docker/docker-compose-cpu-vm.yaml"
 REMOTE_CRUSOE_BEACON_SERVICE="systemctl/crusoe-beacon.service"
-
 SYSTEMCTL_DIR="/etc/systemd/system"
 CRUSOE_BEACON_DIR="/etc/crusoe/beacon"
-
-BEACON_TOKEN_FILE="$CRUSOE_BEACON_DIR/.beacon_token"
 BEACON_TOKEN_LENGTH=82
-BEACON_TOKEN=""
-
-# Temporary directory for downloading and processing files
-TEMP_CONFIG_DIR=$(mktemp -d -t beacon_config_XXXXXX)
+ENV_FILE="$CRUSOE_BEACON_DIR/.env" # Define the .env file path
 
 # --- Helper Functions ---
 
@@ -39,10 +35,6 @@ dir_exists() {
 
 error_exit() {
   echo "Error: $1" >&2
-  # Clean up temporary directory before exiting on error
-  if [[ -d "$TEMP_CONFIG_DIR" ]]; then
-    rm -rf "$TEMP_CONFIG_DIR"
-  fi
   exit 1
 }
 
@@ -72,46 +64,6 @@ install_docker() {
 # Ensure the script is run as root.
 check_root
 
-# Ensure temporary directory is cleaned up on script exit
-trap "rm -rf '$TEMP_CONFIG_DIR'" EXIT
-
-check_os_support
-
-status "Fetching Beacon secret."
-# 1. Try to fetch the token from the BEACON_TOKEN_FILE
-echo "Checking for token in: $BEACON_TOKEN_FILE..."
-if [[ -f "$BEACON_TOKEN_FILE" && -r "$BEACON_TOKEN_FILE" ]]; then
-    BEACON_TOKEN=$(cat "$BEACON_TOKEN_FILE")
-    if [ "${#BEACON_TOKEN}" -ne $BEACON_TOKEN_LENGTH ]; then
-      echo "BEACON_TOKEN should be $BEACON_TOKEN_LENGTH characters long."
-      BEACON_TOKEN=""
-    else
-      echo "Token successfully read from $BEACON_TOKEN_FILE."
-    fi
-else
-  echo "File $BEACON_TOKEN_FILE not found or not readable." >&2
-fi
-
-# 2. If token is not found, prompt the user
-if [[ -z "$BEACON_TOKEN" ]]; then
-  echo "Token not found from file. Prompting user for input..."
-  echo "Please enter your authentication token:"
-  read -s BEACON_TOKEN # -s for silent input (no echo)
-  echo "" # Add a newline after the silent input for better readability
-
-  if [ "${#BEACON_TOKEN}" -ne $BEACON_TOKEN_LENGTH ]; then
-    echo "BEACON_TOKEN should be $BEACON_TOKEN_LENGTH characters long."
-    echo "User Crusoe CLI to get a new token:"
-    echo "Command: crusoe monitoring tokens create"
-    error_exit "BEACON_TOKEN is invalid. "
-  fi
-fi
-
-status "Create Beacon target directory."
-if ! dir_exists "$CRUSOE_BEACON_DIR"; then
-  mkdir -p "$CRUSOE_BEACON_DIR"
-fi
-
 status "Ensure docker installation."
 if command_exists docker; then
   echo "Docker is already installed."
@@ -120,43 +72,20 @@ else
   install_docker
 fi
 
-status "Check if VM has NVIDIA GPUs."
-if lspci | grep -q "NVIDIA Corporation"; then
-  IS_GPU_VM=true
-  echo "NVIDIA GPUs detected."
-else
-  IS_GPU_VM=false
-  echo "NVIDIA GPU not detected."
-fi
-
 # Ensure wget is installed
 status "Ensuring wget is installed."
 if ! command_exists wget; then
   apt-get update && apt-get install -y wget || error_exit "Failed to install wget."
 fi
 
-# Download required files into the temporary directory
-status "Downloading Beacon configuration files from GitHub."
+status "Create Beacon target directory."
+if ! dir_exists "$CRUSOE_BEACON_DIR"; then
+  mkdir -p "$CRUSOE_BEACON_DIR"
+fi
 
-# Download DCGM Exporter Metrics Config
-LOCAL_DCGM_EXPORTER_METRICS_CONFIG="$TEMP_CONFIG_DIR/$(basename $REMOTE_DCGM_EXPORTER_METRICS_CONFIG)"
-wget -q -O "$LOCAL_DCGM_EXPORTER_METRICS_CONFIG" "$GITHUB_RAW_BASE_URL/$REMOTE_DCGM_EXPORTER_METRICS_CONFIG" || error_exit "Failed to download $REMOTE_DCGM_EXPORTER_METRICS_CONFIG"
-
-# Download Vector Config for GPU VM
-LOCAL_VECTOR_CONFIG_GPU_VM="$TEMP_CONFIG_DIR/$(basename $REMOTE_VECTOR_CONFIG_GPU_VM)"
-wget -q -O "$LOCAL_VECTOR_CONFIG_GPU_VM" "$GITHUB_RAW_BASE_URL/$REMOTE_VECTOR_CONFIG_GPU_VM" || error_exit "Failed to download $REMOTE_VECTOR_CONFIG_GPU_VM"
-
-# Download Docker Compose file for GPU VM Ubuntu 22.04
-LOCAL_DOCKER_COMPOSE_GPU_VM_UBUNTU_22="$TEMP_CONFIG_DIR/$(basename $REMOTE_DOCKER_COMPOSE_GPU_VM_UBUNTU_22)"
-wget -q -O "$LOCAL_DOCKER_COMPOSE_GPU_VM_UBUNTU_22" "$GITHUB_RAW_BASE_URL/$REMOTE_DOCKER_COMPOSE_GPU_VM_UBUNTU_22" || error_exit "Failed to download $REMOTE_DOCKER_COMPOSE_GPU_VM_UBUNTU_22"
-
-# Download Crusoe Beacon Service file
-LOCAL_CRUSOE_BEACON_SERVICE="$TEMP_CONFIG_DIR/$(basename $REMOTE_CRUSOE_BEACON_SERVICE)"
-wget -q -O "$LOCAL_CRUSOE_BEACON_SERVICE" "$GITHUB_RAW_BASE_URL/$REMOTE_CRUSOE_BEACON_SERVICE" || error_exit "Failed to download $REMOTE_CRUSOE_BEACON_SERVICE"
-
-
-# install GPU VM dependencies
-if $IS_GPU_VM; then
+# Download required config files
+# if VM has NVIDIA GPUs
+if lspci | grep -q "NVIDIA Corporation"; then
   status "Ensure NVIDIA dependencies exist."
   if command_exists dcgmi && command_exists nvidia-ctk; then
     echo "Required NVIDIA dependencies are already installed."
@@ -164,49 +93,61 @@ if $IS_GPU_VM; then
     error_exit "Cannot find required NVIDIA dependencies. Please install them and try again."
   fi
 
-  status "Copy DCGM exporter metrics config."
-  if ! file_exists "$LOCAL_DCGM_EXPORTER_METRICS_CONFIG"; then
-    error_exit "DCGM exporter metrics config not found in temporary directory: $LOCAL_DCGM_EXPORTER_METRICS_CONFIG"
-  fi
-  echo "cp $LOCAL_DCGM_EXPORTER_METRICS_CONFIG $CRUSOE_BEACON_DIR/dcp-metrics-included.csv"
-  cp "$LOCAL_DCGM_EXPORTER_METRICS_CONFIG" "$CRUSOE_BEACON_DIR/dcp-metrics-included.csv"
+  check_os_support
 
-  status "Copy Vector config."
-  if ! file_exists "$LOCAL_VECTOR_CONFIG_GPU_VM"; then
-    error_exit "Vector config not found in temporary directory: $LOCAL_VECTOR_CONFIG_GPU_VM"
-  fi
-  echo "cp $LOCAL_VECTOR_CONFIG_GPU_VM $CRUSOE_BEACON_DIR/vector.yaml"
-  cp "$LOCAL_VECTOR_CONFIG_GPU_VM" "$CRUSOE_BEACON_DIR/vector.yaml"
+  status "Download DCGM exporter metrics config."
+  wget -q -O "$CRUSOE_BEACON_DIR/dcp-metrics-included.csv" "$GITHUB_RAW_BASE_URL/$REMOTE_DCGM_EXPORTER_METRICS_CONFIG" || error_exit "Failed to download $REMOTE_DCGM_EXPORTER_METRICS_CONFIG"
 
-  status "Copy docker-compose file."
-  if [[ $UBUNTU_OS_VERSION == "22.04" ]]; then
-    if ! file_exists "$LOCAL_DOCKER_COMPOSE_GPU_VM_UBUNTU_22"; then
-      error_exit "Docker compose file not found in temporary directory: $LOCAL_DOCKER_COMPOSE_GPU_VM_UBUNTU_22"
-    fi
-    echo "cp $LOCAL_DOCKER_COMPOSE_GPU_VM_UBUNTU_22 $CRUSOE_BEACON_DIR/docker-compose.yaml"
-    cp "$LOCAL_DOCKER_COMPOSE_GPU_VM_UBUNTU_22" "$CRUSOE_BEACON_DIR/docker-compose.yaml"
-  else
-      error_exit "Ubuntu version $UBUNTU_OS_VERSION is not supported."
-  fi
+  status "Download GPU Vector config."
+  wget -q -O "$CRUSOE_BEACON_DIR/vector.yaml" "$GITHUB_RAW_BASE_URL/$REMOTE_VECTOR_CONFIG_GPU_VM" || error_exit "Failed to download $REMOTE_VECTOR_CONFIG_GPU_VM"
+
+  status "Download GPU docker-compose file."
+  wget -q -O "$CRUSOE_BEACON_DIR/docker-compose.yaml" "$GITHUB_RAW_BASE_URL/$REMOTE_DOCKER_COMPOSE_GPU_VM_UBUNTU_22" || error_exit "Failed to download $REMOTE_DOCKER_COMPOSE_GPU_VM_UBUNTU_22"
+
+# if VM has no NVIDIA GPUs
 else
-  error_exit "Non-GPU VMs are not supported."
+  # The original script had an error_exit here. If CPU VMs are not supported, this should remain.
+  # If they are intended to be supported later, this error_exit should be removed and the following
+  # wget commands for CPU configs would be active.
+  error_exit "Non-GPU VMs are currently not supported."
+  # status "Copy CPU Vector config."
+  # wget -q -O "$CRUSOE_BEACON_DIR/vector.yaml" "$GITHUB_RAW_BASE_URL/$REMOTE_VECTOR_CONFIG_CPU_VM" || error_exit "Failed to download $REMOTE_VECTOR_CONFIG_CPU_VM"
+
+  # status "Copy CPU docker-compose file."
+  # wget -q -O "$CRUSOE_BEACON_DIR/docker-compose.yaml" "$GITHUB_RAW_BASE_URL/$REMOTE_DOCKER_COMPOSE_CPU_VM" || error_exit "Failed to download $REMOTE_DOCKER_COMPOSE_CPU_VM"
 fi
 
-status "Update docker-compose.yaml with BEACON_TOKEN."
-sed -i.bak "s|BEACON_TOKEN_PLACEHOLDER|${BEACON_TOKEN}|g" "$CRUSOE_BEACON_DIR/docker-compose.yaml"
-status "Update docker-compose.yaml with CRUSOE_VM_ID."
-sed -i.bak "s|VM_ID_PLACEHOLDER|${CRUSOE_VM_ID}|g" "$CRUSOE_BEACON_DIR/docker-compose.yaml"
+status "Fetching Beacon auth token."
+if [[ -z "$BEACON_TOKEN" ]]; then
+  echo "Command: crusoe monitoring tokens create"
+  echo "Please enter the crusoe monitoring token:"
+  read -s BEACON_TOKEN # -s for silent input (no echo)
+  echo "" # Add a newline after the silent input for better readability
+
+  if [ "${#BEACON_TOKEN}" -ne $BEACON_TOKEN_LENGTH ]; then
+    echo "BEACON_TOKEN should be $BEACON_TOKEN_LENGTH characters long."
+    echo "Use Crusoe CLI to generate a new token:"
+    echo "Command: crusoe monitoring tokens create"
+    error_exit "BEACON_TOKEN is invalid. "
+  fi
+fi
+
+status "Creating .env file with BEACON_AUTH_TOKEN and VM_ID."
+cat <<EOF > "$ENV_FILE"
+BEACON_AUTH_TOKEN='${BEACON_TOKEN}'
+VM_ID='${CRUSOE_VM_ID}'
+EOF
+echo ".env file created at $ENV_FILE"
+
+status "Download crusoe-beacon.service."
+wget -q -O "$SYSTEMCTL_DIR/crusoe-beacon.service" "$GITHUB_RAW_BASE_URL/$REMOTE_CRUSOE_BEACON_SERVICE" || error_exit "Failed to download $REMOTE_CRUSOE_BEACON_SERVICE"
 
 status "Enable systemctl service for crusoe-beacon."
-if ! file_exists "$LOCAL_CRUSOE_BEACON_SERVICE"; then
-  error_exit "Crusoe Beacon service file not found in temporary directory: $LOCAL_CRUSOE_BEACON_SERVICE"
-fi
-echo "cp $LOCAL_CRUSOE_BEACON_SERVICE $SYSTEMCTL_DIR/crusoe-beacon.service"
-cp "$LOCAL_CRUSOE_BEACON_SERVICE" "$SYSTEMCTL_DIR/crusoe-beacon.service"
 echo "systemctl daemon-reload"
 systemctl daemon-reload
 echo "systemctl enable crusoe-beacon.service"
 systemctl enable crusoe-beacon.service
 
 status "Setup Complete!"
+echo "Run: 'systemctl start crusoe-beacon.service' to push metrics to Crusoe."
 echo "Setup script finished successfully!"
