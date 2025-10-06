a855ae69be105ed79347c17efe379225063a2523
Ashish Rathi <arathi@crusoeenergy.com>
2025-10-06 15:49:02 -0700

    compute nodepool for host metrics in cluster

--- vector-config-reloader/app/vector_config_reloader_app.py
+++ vector-config-reloader/app/vector_config_reloader_app.py
@@ -16,6 +16,10 @@ host_prefix = get(parts, [0]) ?? ""
 prefix_parts, _ = split(host_prefix, "-")
 nodepool_id_parts, _ = slice(prefix_parts, 0, length(prefix_parts) - 1)
 .tags.nodepool, _ = join(nodepool_id_parts, "-")
+} else if exists(.tags.Host) {
+prefix_parts, _ = split(.tags.Host, "-")
+nodepool_id_parts, _ = slice(prefix_parts, 0, length(prefix_parts) - 1)
+.tags.nodepool, _ = join(nodepool_id_parts, "-")
 }
 .tags.cluster_id = "${CRUSOE_CLUSTER_ID}"
 .tags.vm_id = "${VM_ID}"
